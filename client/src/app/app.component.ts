5669fbe7c2cced21c75213b35a50373e5184d40a
spiceydev <spiceydev@gmail.com>
2021-09-10 00:18:09 +1200

    added some types

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: any[];
+  products: IProduct[];
 
   constructor(private http: HttpClient) {
     this.products = [];
   }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: any) => {
-        this.products = response.data;
-      },
-      (error) => console.error(error)
-    );
+    this.http
+      .get<IPagination>('https://localhost:5001/api/products?pageSize=50')
+      .subscribe(
+        (response) => {
+          console.log(`response`, response);
+          this.products = response.data;
+        },
+        (error) => console.error(error)
+      );
   }
 }
