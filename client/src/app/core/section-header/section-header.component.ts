661e8b07d2e3b2f1ecfe8d87b0185e74c304c976
spiceydev <spiceydev@gmail.com>
2021-09-12 05:51:23 +1200

    added breadcrumbs

--- client/src/app/core/section-header/section-header.component.ts
+++ client/src/app/core/section-header/section-header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-section-header',
   templateUrl: './section-header.component.html',
-  styleUrls: ['./section-header.component.scss']
+  styleUrls: ['./section-header.component.scss'],
 })
 export class SectionHeaderComponent implements OnInit {
+  breadcrumb$: Observable<any[]> | null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private bcService: BreadcrumbService) {
+    this.breadcrumb$ = null;
   }
 
+  ngOnInit(): void {
+    this.breadcrumb$ = this.bcService.breadcrumbs$;
+  }
 }
